382e997e381c54c2348b37cfa6fc7fbe5090c6ae
Luís-Felipe <luis.felipeoliveirafrancisco@gmail.com>
2023-11-02 15:05:46 -0300

    update

--- pages/home.php
+++ pages/home.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Flora Trade</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/luxonauta/luxa@1.1/dist/compressed/luxa.css">
+  <link rel="stylesheet" href="../css/style.css">
 </head>
 
 <body>
