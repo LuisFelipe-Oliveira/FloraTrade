67e86ef77117ea023cc5c3aeb24ce832b92e690b
Luís-Felipe <luis.felipeoliveirafrancisco@gmail.com>
2023-11-09 16:09:45 -0300

    correction

--- public/config/connect.php
+++ public/config/connect.php
@@ -11,10 +11,6 @@ $password = "";
 try {
   $conn = new PDO("mysql:host=$host;dbname=$data_base", $user, $password);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    if($conn)
-    {
-      echo "Conectado!";
-    }
 } catch (PDOException $th) {
   echo "erro na conexão: " . $th->getMessage();
 }
