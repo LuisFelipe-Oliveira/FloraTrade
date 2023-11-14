df2c895658788fec9058982f0cce0d9209167efd
JVictor5 <josevictor_caravantealves10@hotmail.com>
2023-11-14 16:24:26 -0300

    update

--- public/footer.php
+++ public/footer.php
@@ -1,9 +1,17 @@
 <hr class="hr-gray" />
-  <div class="footer">
-    <h5>É OS GURIS E NÃO TEM JEITO</h5>
-    <p>Fatec Presidente Prudente</p>
-    <p>Eletiva Programação Web</p>
-    <p>Professor Paulo Tacca</p>
-    <p>© Copyright 2023</p>
-  </div>
-  <hr class="hr-gray" />
+<div class="footer">
+  <h5>É OS GURIS E NÃO TEM JEITO</h5>
+  <p>Fatec Presidente Prudente</p>
+  <p>Eletiva Programação Web</p>
+  <p>Professor Paulo Tacca</p>
+  <p>© Copyright 2023</p>
+</div>
+<hr class="hr-gray" />
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+  integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<script src="
+https://cdn.jsdelivr.net/npm/sweetalert2@11.9.0/dist/sweetalert2.all.min.js
+"></script>
+<script src="assets/js/script.js"></script>
+<script src="assets/js/alert.js"></script>
---
+++ public/header.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/luxonauta/luxa@1.1/dist/compressed/luxa.css" />
+    <link rel="stylesheet" href="assets\css\style.css" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.7/css/all.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/sweetalert2@11.9.0/dist/sweetalert2.min.css" rel="stylesheet">
+    <title>Flora Trade</title>
+</head>
+
+<body>
+
+    <div class="alinhamento">
+        <!-- header -->
+        <div class="header">
+            <!-- menu lateral -->
+            <div class="menu">
+                <button class="btn-menu-open" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight"
+                    aria-controls="offcanvasRight"><i class="fas fa-bars" fill="currentColor"></i></button>
+                <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight"
+                    aria-labelledby="offcanvasRightLabel">
+                    <div class="offcanvas-header">
+                        <h5 class="offcanvas-title" id="offcanvasRightLabel">Menu</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    </div>
+                    <hr class="hr-green">
+                    <div class="offcanvas-body">
+                        <ul class="itens-menu">
+                            <a href="">
+                                <li class="button">Entrar</li>
+                            </a>
+                            <a href="">
+                                <li class="button">Cadastrar</li>
+                            </a>
+                            <a onclick="error()">
+                                <li>Usuários</li>
+                            </a>
+                            <a href="tabelaProduto.php">
+                                <li>Produtos</li>
+                            </a>
+                            <a onclick="error()">
+                                <li>Estoque</li>
+                            </a>
+                            <a onclick="error()">
+                                <li>Clientes</li>
+                            </a>
+                            <a onclick="error()">
+                                <li>Fornecedores</li>
+                            </a>
+                            <a onclick="error()">
+                                <li>Contatos</li>
+                            </a>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <!-- botões de login -->
+            <div class="btn-login">
+                <div class="btn-entrar">
+                    <h4><a href="">Entrar</a></h4>
+                </div>
+                <div class="btn-cadastrar">
+                    <h4><a href="">Cadastrar</a></h4>
+                </div>
+            </div>
+            <!-- Logo -->
+            <div class="logo">
+                <img class="logo" src="assets/imgs/Logo.png" alt="" />
+            </div>
+        </div>
+    </div>
+    <hr class="hr-green" />
