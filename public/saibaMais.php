84f647c383dcfdfda475e4724fc108acba3a40eb
JVictor5 <josevictor_caravantealves10@hotmail.com>
2023-11-23 15:34:01 -0300

    :lipstick: feat: mudança no tamanho da fonte

--- public/saibaMais.php
+++ public/saibaMais.php
@@ -6,7 +6,7 @@ require_once("header.php");
 
 <div class="">
     <div class="Centraliza">
-        <h3>Bem-vindo ao novo sistema de gestão da nossa floricultura, desenvolvido para otimizar e simplificar nossas
+        <p>Bem-vindo ao novo sistema de gestão da nossa floricultura, desenvolvido para otimizar e simplificar nossas
             operações
             diárias. Fortalecendo a gestão de mais de 200 floriculturas por todo o Brasil, nós prezamos em oferecer um
             serviço
@@ -15,7 +15,7 @@ require_once("header.php");
             um
             Escopo de Especificações de Requisitos (ERS) abrangente, visando melhorar a gestão do estoque e proporcionar
             maior
-            praticidade no controle de caixa.</h3>
+            praticidade no controle de caixa.</p>
         <br>
     </div>
     <div class="linha-com-icone-container">
@@ -33,12 +33,12 @@ require_once("header.php");
     </div>
 
     <div class="Centraliza">
-        <h3>Principais Características:</h3>
+        <p>Principais Características:</p>
     </div>
     <div class="Color">
         <div class="conteudo aling-strech">
             <div class="card-esquerdo">
-                <h3>Controle de Estoque Automatizado: Todos os produtos da nossa floricultura são identificados por
+                <p>Controle de Estoque Automatizado: Todos os produtos da nossa floricultura são identificados por
                     códigos
                     numéricos e códigos de barras exclusivos. Isso facilita a rastreabilidade e agiliza o processo
                     de
@@ -47,7 +47,7 @@ require_once("header.php");
                     necessário
                     para
                     verificar as vendas e a disponibilidade de produtos. Essa funcionalidade proporciona eficiência
-                    operacional, liberando mais tempo para outras atividades essenciais.</h3>
+                    operacional, liberando mais tempo para outras atividades essenciais.</p>
             </div>
             <div class="card-IMGdireito">
                 <img src="assets\imgs\floriculturaIMG3.jpg" alt="" />
@@ -56,12 +56,12 @@ require_once("header.php");
     </div>
     <div class="conteudo aling-strech">
         <div class="card-direito">
-            <h3>Controle de Caixa Simplificado:
+            <p>Controle de Caixa Simplificado:
                 Integração de funções básicas de controle de caixa para automatizar processos financeiros
                 essenciais.
                 Facilidade na geração de relatórios de vendas, lucros e prejuízos mensais, proporcionando uma visão
                 clara e
-                detalhada do desempenho financeiro do negócio.</h3>
+                detalhada do desempenho financeiro do negócio.</p>
         </div>
         <div class="card-IMGesquerdo">
             <img src="assets\imgs\floriculturaIMG4.png" alt="" />
@@ -70,7 +70,7 @@ require_once("header.php");
     <div class="Color">
         <div class="conteudo aling-strech">
             <div class="card-esquerdo">
-                <h3>Gestão de Plantas e Flores:
+                <p>Gestão de Plantas e Flores:
                     Funcionalidades adicionais incluem informações sobre a saúde das plantas e flores, evitando
                     problemas
                     como
@@ -78,7 +78,7 @@ require_once("header.php");
                     O sistema fornece lembretes automáticos e resumos detalhados sobre cuidados específicos,
                     contribuindo
                     para a
-                    preservação da qualidade dos produtos.</h3>
+                    preservação da qualidade dos produtos.</p>
             </div>
 
             <div class="card-IMGdireito">
@@ -89,11 +89,11 @@ require_once("header.php");
 
     <div class="conteudo aling-strech">
         <div class="card-direito">
-            <h3>Visualização Simplificada:
+            <p>Visualização Simplificada:
                 Resumos intuitivos dos dados de vendas estão disponíveis para facilitar a análise da performance
                 mensal.
                 Acesso rápido a informações cruciais, permitindo tomadas de decisão mais informadas e estratégicas.
-            </h3>
+            </p>
         </div>
         <div class="card-IMGesquerdo">
             <img src="assets\imgs\floriculturaIMG2.jpg" alt="" />
@@ -115,12 +115,12 @@ require_once("header.php");
     </div>
 
     <div class="Centraliza">
-        <h3>Ao adotar nosso sistema integrado de gestão, estamos comprometidos em aprimorar a eficiência operacional
+        <p>Ao adotar nosso sistema integrado de gestão, estamos comprometidos em aprimorar a eficiência operacional
             da
             nossa floricultura. Valorizamos a sua parceria e esperamos que essa ferramenta contribua para uma
             experiência
             ainda mais positiva, tanto para nossa equipe quanto para nossos clientes. Seja bem-vindo à era da gestão
-            simplificada e eficiente na nossa floricultura.</h3>
+            simplificada e eficiente na nossa floricultura.</p>
     </div>
 </div>
 <?php
